dcb200824c9e7273e6b533ee173e3ad6b450d170
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-10-01 14:03:19 +0200

    feat: add cache status to projector api

---
+++ src/PostalRegistry.Projector/Caches/CachesController.cs
@@ -0,0 +1,47 @@
+namespace PostalRegistry.Projector.Caches
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Threading;
+    using Be.Vlaanderen.Basisregisters.Api;
+    using Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+
+    [ApiVersion("1.0")]
+    [ApiRoute("caches")]
+    public class CachesController : ApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get(
+            [FromServices] IConfiguration configuration,
+            [FromServices] LastChangedListContext lastChangedListContext,
+            CancellationToken cancellationToken)
+        {
+            var maxErrorCount = configuration.GetValue<int?>("Caches:LastChangedList:MaxErrorCount") ?? 10;
+            var maxErrorTimeInSeconds =
+                configuration.GetValue<int?>("Caches:LastChangedList:MaxErrorTimeInSeconds") ?? 60;
+
+            var maxErrorTime = DateTimeOffset.UtcNow.AddSeconds(-1 * maxErrorTimeInSeconds);
+            var numberOfRecords = await lastChangedListContext
+                .LastChangedList
+                .OrderBy(x => x.Id)
+                .Where(r =>
+                    r.Position > r.LastPopulatedPosition &&
+                    r.ErrorCount < maxErrorCount &&
+                    (r.LastError == null || r.LastError < maxErrorTime))
+                .CountAsync(cancellationToken);
+
+            return Ok(new[]
+            {
+                new
+                {
+                    name = "LastChangedList",
+                    numberOfRecordsToProcess = numberOfRecords
+                }
+            });
+        }
+    }
+}
