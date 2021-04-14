1b3168c67599635350fe0e1eba78025cac0dc281
Arne Dumarey <dumarey.arne@gmail.com>
2021-04-14 20:17:09 +0200

    style: clean up

--- src/PostalRegistry.Api.Legacy/PostalInformation/PostalInformationController.cs
+++ src/PostalRegistry.Api.Legacy/PostalInformation/PostalInformationController.cs
@@ -3,7 +3,6 @@ namespace PostalRegistry.Api.Legacy.PostalInformation
     using System;
     using System.Linq;
     using System.Net.Mime;
-    using System.Reflection;
     using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
