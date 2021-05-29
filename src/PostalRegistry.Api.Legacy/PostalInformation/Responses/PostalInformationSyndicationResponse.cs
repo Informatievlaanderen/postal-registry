4cc296227f0f147f3ac3e856676f1eb85d255df8
Arne Dumarey <dumarey.arne@gmail.com>
2021-05-29 07:52:17 +0200

    fix: move to 5.0.6

--- src/PostalRegistry.Api.Legacy/PostalInformation/Responses/PostalInformationSyndicationResponse.cs
+++ src/PostalRegistry.Api.Legacy/PostalInformation/Responses/PostalInformationSyndicationResponse.cs
@@ -186,8 +186,8 @@ namespace PostalRegistry.Api.Legacy.PostalInformation.Responses
     <rights>Gratis hergebruik volgens https://overheid.vlaanderen.be/sites/default/files/documenten/ict-egov/licenties/hergebruik/modellicentie_gratis_hergebruik_v1_0.html</rights>
     <updated>2020-09-17T09:14:41Z</updated>
     <author>
-        <name>agentschap Informatie Vlaanderen</name>
-        <email>informatie.vlaanderen@vlaanderen.be</email>
+        <name>Digitaal Vlaanderen</name>
+        <email>digitaal.vlaanderen@vlaanderen.be</email>
     </author>
     <link href=""https://api.basisregisters.vlaanderen.be/v1/feeds/postinfo"" rel=""self"" />
     <link href=""https://api.basisregisters.vlaanderen.be/v1/feeds/postinfo.atom"" rel=""alternate"" type=""application/atom+xml"" />
--- src/PostalRegistry.Projector/Infrastructure/Startup.cs
+++ src/PostalRegistry.Projector/Infrastructure/Startup.cs
@@ -74,8 +74,8 @@ namespace PostalRegistry.Projector.Infrastructure
                             Description = GetApiLeadingText(description),
                             Contact = new OpenApiContact
                             {
-                                Name = "Informatie Vlaanderen",
-                                Email = "informatie.vlaanderen@vlaanderen.be",
+                                Name = "Digitaal Vlaanderen",
+                                Email = "digitaal.vlaanderen@vlaanderen.be",
                                 Url = new Uri("https://legacy.basisregisters.vlaanderen")
                             }
                         },
@@ -180,7 +180,7 @@ namespace PostalRegistry.Projector.Infrastructure
                     Server =
                     {
                         PoweredByName = "Vlaamse overheid - Basisregisters Vlaanderen",
-                        ServerName = "agentschap Informatie Vlaanderen"
+                        ServerName = "Digitaal Vlaanderen"
                     },
                     MiddlewareHooks =
                     {
