a26385014cb613997aea5a2780d2d1035d452f51
Arne Dumarey <dumarey.arne@gmail.com>
2022-04-04 09:06:17 +0200

    fix: set oslo context type to string GAWR-2931

--- src/PostalRegistry.Api.Oslo/PostalInformation/Responses/PostalInformationListOsloResponse.cs
+++ src/PostalRegistry.Api.Oslo/PostalInformation/Responses/PostalInformationListOsloResponse.cs
@@ -3,7 +3,6 @@ namespace PostalRegistry.Api.Oslo.PostalInformation.Responses
     using System;
     using System.Collections.Generic;
     using System.Runtime.Serialization;
-    using Be.Vlaanderen.Basisregisters.Api.JsonConverters;
     using Be.Vlaanderen.Basisregisters.GrAr.Common;
     using Be.Vlaanderen.Basisregisters.GrAr.Legacy;
     using Be.Vlaanderen.Basisregisters.GrAr.Legacy.PostInfo;
@@ -20,8 +19,7 @@ namespace PostalRegistry.Api.Oslo.PostalInformation.Responses
         /// </summary>
         [DataMember(Name = "@context", Order = 0)]
         [JsonProperty(Required = Required.DisallowNull)]
-        [JsonConverter(typeof(PlainStringJsonConverter))]
-        public object Context { get; set; }
+        public string Context { get; set; }
 
         /// <summary>
         /// De verzameling van postcodes.
