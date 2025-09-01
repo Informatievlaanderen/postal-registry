a19a2bd44f1ac7b13429a1c79a9f55a262bcb470
Arne Dumarey <dumarey.arne@gmail.com>
2025-09-01 15:02:15 +0200

    feat(import): add remove postalinfo endpoint

---
+++ src/PostalRegistry.Api.Import/PostalInformationController-RemovePostalInformation.cs
@@ -0,0 +1,41 @@
+﻿namespace PostalRegistry.Api.Import
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.AggregateSource;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Be.Vlaanderen.Basisregisters.CommandHandling.Idempotency;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using PostalInformation.Commands;
+
+    public sealed partial class PostalInformationController
+    {
+        [HttpDelete("{postcode}")]
+        public async Task<IActionResult> RemovePostalInformation(
+            [FromRoute(Name = "postcode")]string? postalCode,
+            [FromServices] IIdempotentCommandHandler idempotentCommandHandler,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var command = new DeletePostalInformation(
+                    new PostalCode(postalCode!)
+                    , CreateProvenance("Verwijder postinformatie"));
+
+                await idempotentCommandHandler.Dispatch(
+                    command.CreateCommandId(),
+                    command,
+                    new Dictionary<string, object>(),
+                    cancellationToken);
+
+                return Ok();
+            }
+            catch (AggregateNotFoundException)
+            {
+                throw new ApiException("Onbestaande postcode", StatusCodes.Status404NotFound);
+            }
+        }
+    }
+}
