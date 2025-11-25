233e4271afe1f699a13c762a90bfa762471ec292
ArneD <dumarey.arne@gmail.com>
2025-11-25 16:36:46 +0100

    feat: remove ldes GAWR-7170

--- src/PostalRegistry.Infrastructure/Schema.cs
+++ src/PostalRegistry.Infrastructure/Schema.cs
@@ -11,7 +11,6 @@ namespace PostalRegistry.Infrastructure
 
         public const string Producer = "PostalRegistryProducer";
         public const string ProducerSnapshotOslo = "PostalRegistryProducerSnapshotOslo";
-        public const string ProducerLdes = "PostalRegistryProducerLdes";
 
         public const string Integration = "integration_postal";
     }
@@ -25,7 +24,6 @@ namespace PostalRegistry.Infrastructure
 
         public const string Producer = "__EFMigrationsHistoryProducer";
         public const string ProducerSnapshotOslo = "__EFMigrationsHistoryProducerSnapshotOslo";
-        public const string ProducerLdes = "__EFMigrationsHistoryProducerLdes";
 
         public const string Integration = "__EFMigrationsHistory";
     }
