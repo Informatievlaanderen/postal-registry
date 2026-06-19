30ac11c3c449dfdf8deefc1f6f5f165334db9c0b
Arne Dumarey <dumarey.arne@gmail.com>
2026-06-19 16:07:18 +0200

    fix(projector): resume projections

--- src/PostalRegistry.Projector/Infrastructure/Startup.cs
+++ src/PostalRegistry.Projector/Infrastructure/Startup.cs
@@ -183,7 +183,7 @@ namespace PostalRegistry.Projector.Infrastructure
             appLifetime.ApplicationStopping.Register(() => _projectionsCancellationTokenSource.Cancel());
             appLifetime.ApplicationStarted.Register(() =>
             {
-                var projectionsManager = serviceProvider.GetRequiredService<IConnectedProjectionsManager>();
+                var projectionsManager = app.ApplicationServices.GetRequiredService<IConnectedProjectionsManager>();
                 projectionsManager.Resume(_projectionsCancellationTokenSource.Token);
             });
         }
